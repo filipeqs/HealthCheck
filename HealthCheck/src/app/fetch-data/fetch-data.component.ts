fb82e27e80b6334517dda89b3c556f6141b6651c
Filipe Silva <filipeqs@outlook.com>
2023-02-10 09:27:17 -0500

    fixed proxy

--- HealthCheck/src/app/fetch-data/fetch-data.component.ts
+++ HealthCheck/src/app/fetch-data/fetch-data.component.ts
@@ -3,27 +3,25 @@ import { Component } from '@angular/core';
 import { environment } from 'src/environments/environment';
 
 @Component({
-  selector: 'app-fetch-data',
-  templateUrl: './fetch-data.component.html',
-  styleUrls: ['./fetch-data.component.css'],
+    selector: 'app-fetch-data',
+    templateUrl: './fetch-data.component.html',
+    styleUrls: ['./fetch-data.component.css'],
 })
 export class FetchDataComponent {
-  public forecasts?: WeatherForecast[];
-  constructor(http: HttpClient) {
-    http
-      .get<WeatherForecast[]>(environment.baseUrl + 'api/weatherforecast')
-      .subscribe({
-        next: (result) => {
-          this.forecasts = result;
-        },
-        error: (error) => console.error(error),
-      });
-  }
+    public forecasts?: WeatherForecast[];
+    constructor(http: HttpClient) {
+        http.get<WeatherForecast[]>(environment.baseUrl + 'api/weatherforecast').subscribe({
+            next: (result) => {
+                this.forecasts = result;
+            },
+            error: (error) => console.error(error),
+        });
+    }
 }
 
 interface WeatherForecast {
-  date: string;
-  temperatureC: number;
-  temperatureF: number;
-  summary: string;
+    date: string;
+    temperatureC: number;
+    temperatureF: number;
+    summary: string;
 }
--- HealthCheck/src/proxy.conf.js
+++ HealthCheck/src/proxy.conf.js
@@ -1,9 +1,9 @@
 const PROXY_CONFIG = [
-  {
-    context: ["/api"],
-    target: "https://localhost:40443",
-    secure: false,
-  },
+    {
+        context: ['/api'],
+        target: 'https://localhost:40443',
+        secure: false,
+    },
 ];
 
 module.exports = PROXY_CONFIG;
